00b7eda7dbcb7be02bed6b3c2c66270d4c9a45ea
Vouts <thevouts@gmail.com>
2017-12-09 18:52:51 +0200

    V1.0

--- app/src/main/java/com/example/android/counterapp/MainActivity.java
+++ app/src/main/java/com/example/android/counterapp/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.android.counterapp;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Define Global variables
+     */
+
+    int scorePlayerA, scorePlayerB;
+    boolean selectedPlayerA = false, selectedPlayerB = false;
+
+    private void initialization (){
+        selectedPlayerA = true;
+
+        LinearLayout li=(LinearLayout) findViewById(R.id.playerA);
+        li.setBackgroundResource(R.color.currentPlayerBG);;
+
+        TextView player = (TextView)findViewById(R.id.namePlayerA);
+        player.setCompoundDrawablesWithIntrinsicBounds( R.drawable.play, 0, 0, 0);
+
+        Button selectedButton = (Button) findViewById(R.id.plusOne);
+        selectedButton.setBackgroundResource(R.color.colorPrimaryDark);;
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initialization();
     }
 }
